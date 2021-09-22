42750d405061d7517bf6bd6c54363e1e6693bb59
Nirbhay Maitra <60140561+NirbhayMaitra@users.noreply.github.com>
2021-09-22 15:14:32 +0530

    Codes

---
+++ task4.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    string name;
+    float basePay,salary;
+    int time;
+    cout<<"Enter name of the employee\n";
+    cin>>name;
+    cout<<"Enter base pay of the employee\n";
+    cin>>basePay;
+    cout<<"Enter number of hours worked by the employee\n";
+    cin>>time;
+
+    cout<<"Name:: "<<name<<endl;
+    cout<<"Base Pay:: "<<basePay<<endl;
+    cout<<"Hours Worked:: "<<time<<endl;
+    if(time>40)
+    {
+        salary=(basePay*40)+((time-40)*(basePay*2));
+    }
+    else
+    {
+        salary=basePay*time;
+    }
+    cout<<"Salary:: "<<salary<<endl;
+    return 0;
+}
